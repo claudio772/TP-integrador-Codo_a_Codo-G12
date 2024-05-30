09827c7ca44ef651f86ad34dfea44da96126b3c3
Alejo <alejohurtadotesta@gmail.com>
2024-05-30 13:14:32 -0300

    Se crea condicional para no mostrar las etiquetas con valor null. Se crea la pagina catalogo para mostrar todas las bebidas.

--- js/apiBebidas.js
+++ js/apiBebidas.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const search = document.querySelector('#buscadorBebidas');
-  const bebidasHTML = document.querySelector('#bebidas');
+document.addEventListener("DOMContentLoaded", () => {
+  const search = document.querySelector("#buscadorBebidas");
+  const bebidasHTML = document.querySelector("#bebidas");
 
   const llamadaAPI = () => {
-    const inputBebida = document.querySelector('#inputBuscador').value;
-    if (inputBebida.trim() === '') {
+    const inputBebida = document.querySelector("#inputBuscador").value;
+    if (inputBebida.trim() === "") {
       Swal.fire({
-        title: 'El campo esta vacio',
-        text: 'Coloca el nombre de tu bebida favorita!',
-        icon: 'question',
+        title: "El campo esta vacio",
+        text: "Coloca el nombre de tu bebida favorita!",
+        icon: "question",
       });
       return;
     }
@@ -22,16 +22,16 @@ document.addEventListener('DOMContentLoaded', () => {
           cargarBebidas(data.drinks);
         } else {
           Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Algo salio mal! No se encontraron bebidas con ese nombre',
+            icon: "error",
+            title: "Oops...",
+            text: "Algo salio mal! No se encontraron bebidas con ese nombre",
           });
         }
       })
-      .catch((error) => console.log('Error encontrado: ', error));
+      .catch((error) => console.log("Error encontrado: ", error));
   };
 
-  search.addEventListener('submit', (event) => {
+  search.addEventListener("submit", (event) => {
     event.preventDefault();
     llamadaAPI();
   });
@@ -50,7 +50,48 @@ document.addEventListener('DOMContentLoaded', () => {
         strIngredient2,
         strIngredient3,
         strIngredient4,
+        strIngredient5,
+        strIngredient6,
+        strIngredient7,
+        strIngredient8,
+        strIngredient9,
+        strIngredient10,
+        strIngredient11,
+        strIngredient12,
+        strIngredient13,
+        strIngredient14,
+        strIngredient15,
       } = element;
+
+      // crea una variable para almacenar el html de cada ingrediente
+      let ingredientesHTML = "";
+      // crea una lista con todos los ingredientes que puede contener
+      const ingredientes = [
+        strIngredient1,
+        strIngredient2,
+        strIngredient3,
+        strIngredient4,
+        strIngredient5,
+        strIngredient6,
+        strIngredient7,
+        strIngredient8,
+        strIngredient9,
+        strIngredient10,
+        strIngredient11,
+        strIngredient12,
+        strIngredient13,
+        strIngredient14,
+        strIngredient15,
+      ];
+      // recorremos la lista de los ingredientes
+      ingredientes.forEach((ingrediente) => {
+        // elavuamos cada ingrediente, asegurandonos de que no sean "null" y contengan un valor
+        if (ingrediente) {
+          // si el ingrediente contiene un valor, se agrega a una etiqueta HTML y diha etiqueta se agrega y se suma junto con las otras, dentro de la variable ya creada anteriormente
+          ingredientesHTML += `<li>${ingrediente}</li>`;
+        }
+      });
+
       return (bebidasHTML.innerHTML += `
       <div class="bebida">
             <div class="bebida-principal d-flex">
@@ -69,13 +110,10 @@ document.addEventListener('DOMContentLoaded', () => {
               </div>
             </div>
             <p>${strGlass}</p>
-            <h3>estilo: ${strIBA}</h3>
+            ${strIBA ? `<h3>estilo: ${strIBA}</h3>` : ""}
             <h4>Ingredientes:</h4>
             <ul>
-              <li>${strIngredient1}</li>
-              <li>${strIngredient2}</li>
-              <li>${strIngredient3}</li>
-              <li>${strIngredient4}</li>
+              ${ingredientesHTML}
             </ul>
           </div>`);
     });
